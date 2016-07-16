2dc4938a5e1530bc509eb57a281e174b8390f141
readysetlearn <mister_josh@ymail.com>
2016-07-16 04:27:37 -0700

    Used reflection to access private method to test.

--- reflection/ShorthandExpanderTest.java
+++ reflection/ShorthandExpanderTest.java
@@ -3,12 +3,29 @@ package reflection;
 /*static import allows for unqualified calls to static methods*/
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import java.lang.reflect.*;
 
 public class ShorthandExpanderTest {
 	@Test
 	public void with() {
 		String input = "This is a test w/, \"w/\"";
-		String result = ShorthandExpander.repWith(input);
-		assertEquals("This is a test with, \"with\"", result);
+		
+		try {
+			
+			/*Use reflection*/
+			Method meth = ShorthandExpander.class.getDeclaredMethod("repWith", String.class);
+			meth.setAccessible(true);
+			Object obj = meth.invoke(null, input);
+			String result = (String)obj;
+			System.out.println("This is result: "+result);
+			assertEquals("This is a test with, \"with\"", result);
+			
+		} catch(IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+
 	}
 }
